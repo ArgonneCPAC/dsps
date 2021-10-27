a671a379f8d06a84963c1c0ae77cc8a879b71045
Andrew Hearin <ahearin@anl.gov>
2021-10-27 11:19:30 -0500

    Added unit test of linear SFR(t) age weights

--- dsps/tests/test_stellar_ages.py
+++ dsps/tests/test_stellar_ages.py
@@ -10,6 +10,14 @@ from ..utils import _jax_get_dt_array
 FSPS_LG_AGES = np.arange(5.5, 10.2, 0.05)  # log10 ages in years
 
 
+def linear_sfr(t_gyr):
+    return t_gyr * 1e9
+
+
+def linear_smh(t0, t_gyr):
+    return 1e9 * 0.5 * (t_gyr ** 2 - t0 ** 2)
+
+
 def test_age_bin_edges_have_correct_array_shape():
     lgt_ages = np.linspace(5.5, 10.5, 50)
     lgt_age_bins = _get_lg_age_bin_edges(lgt_ages)
@@ -58,3 +66,28 @@ def test_age_weights_agree_with_analytical_calculation_of_constant_sfr_weights()
         lgt_birth_bin_edges, lgt_table, logsm_table
     )
     assert np.allclose(dsps_age_weights, correct_weights, atol=0.01)
+
+
+def test_age_weights_agree_with_analytical_calculation_of_linear_sfr_weights():
+
+    t_obs = 16.0
+
+    # Analytically calculate age distributions for SFR(t) = t
+    log_ages_gyr = FSPS_LG_AGES - 9
+    lgt_age_bin_edges = _get_lg_age_bin_edges(log_ages_gyr)
+    t_age_bin_edges_gyr = 10 ** lgt_age_bin_edges
+    t_births_bin_edges = t_obs - t_age_bin_edges_gyr
+    mstar_at_age_bins = linear_smh(T_BIRTH_MIN, t_births_bin_edges)
+    dmstar_ages = -np.diff(mstar_at_age_bins)
+    correct_weights = dmstar_ages / dmstar_ages.sum()
+
+    # Calculate age distributions with DSPS
+    t_table = np.linspace(T_BIRTH_MIN, t_obs, 50_000)
+    lgt_table = np.log10(t_table)
+
+    logsm_table = np.log10(linear_smh(T_BIRTH_MIN, t_table[1:]))
+    lgt_birth_bin_edges = _get_lgt_birth(t_obs, lgt_age_bin_edges)
+    dsps_age_weights = _get_age_weights_from_tables(
+        lgt_birth_bin_edges, lgt_table[1:], logsm_table
+    )
+    assert np.allclose(dsps_age_weights, correct_weights, atol=0.001)
