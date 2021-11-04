dd5afa2e6041858434b1de39787e7aeae0754e7e
Andrew Hearin <ahearin@anl.gov>
2021-11-04 18:08:58 -0500

    Added calc_obs_mags_galpop convenience function

--- dsps/photometry.py
+++ dsps/photometry.py
@@ -27,9 +27,12 @@ _calc_obs_mag_vmap = jjit(
 _a = (*[None] * 4, 0, *[None] * 5)
 _calc_obs_mag_vmap_z = jjit(jvmap(_calc_obs_mag, in_axes=_a))
 
+_b = (None, 0, *[None] * 8)
+_calc_obs_mag_vmap_spec = jjit(jvmap(_calc_obs_mag, in_axes=_b))
+
 
 @jjit
-def calc_obs_mag_history(
+def calc_obs_mag_history_singlegal(
     wave_spec, lum_spec, wave_filter, trans_filter, z_obs, Om0, Ode0, w0, wa, h
 ):
     """Calculate the history of the observed flux of a single galaxy
@@ -73,6 +76,56 @@ def calc_obs_mag_history(
 
     """
     obs_mags = _calc_obs_mag_vmap_z(
+        wave_spec, lum_spec, wave_filter, trans_filter, z_obs, Om0, Ode0, w0, wa, h
+    )
+    return obs_mags
+
+
+@jjit
+def calc_obs_mags_galpop(
+    wave_spec, lum_spec, wave_filter, trans_filter, z_obs, Om0, Ode0, w0, wa, h
+):
+    """Calculate the history of the observed flux of a galaxy population
+    at a single redshift observed through a particular filter.
+
+    Parameters
+    ----------
+    wave_spec_rest : ndarray of shape (n_wave_spec, )
+        Rest-frame wavelengths of the spectrum
+
+    lum_spec : ndarray of shape (n_gals, n_wave_spec)
+        Spectrum of each galaxy in Lsun/Hz
+
+    wave_filter : ndarray of shape (n_wave_filter, )
+        Wave length of the filter transmission curve
+
+    trans_filter : ndarray of shape (n_wave_filter, )
+        Fraction of the incident flux transmitted through the filter
+
+    z_obs : float
+        Redshift of the observed galaxies
+
+    Om0: float
+        Omega matter at z=0
+
+    Ode0: float
+        Omega DE at z=0
+
+    w0 : float
+        DE eqn of state today
+
+    wa : float
+        DE eqn of state deriv
+
+    h : float
+        Little h
+
+    Returns
+    -------
+    obs_mags : ndarray of shape (n_gals, )
+
+    """
+    obs_mags = _calc_obs_mag_vmap_spec(
         wave_spec, lum_spec, wave_filter, trans_filter, z_obs, Om0, Ode0, w0, wa, h
     )
     return obs_mags
