35ddda0ad1cf09c36088911a6df15ac303a5287b
Andrew Hearin <ahearin@anl.gov>
2021-11-16 13:39:06 -0600

    Added calculation of obs_mag with dust

--- dsps/diffstar_photometry_kernels.py
+++ dsps/diffstar_photometry_kernels.py
@@ -1,7 +1,7 @@
 """
 """
 from jax import jit as jjit
-from .photometry_kernels import _calc_rest_mag
+from .photometry_kernels import _calc_rest_mag, _calc_obs_mag
 from .weighted_ssps import _calc_weighted_ssp_from_diffstar_params_const_zmet
 from .attenuation_kernels import _flux_ratio, sbl18_k_lambda, RV_C00
 
@@ -111,3 +111,69 @@ def _calc_weighted_rest_mag_from_diffstar_params_const_zmet_dust(
 
     rest_mag = _calc_rest_mag(spec_wave, attenuated_ssp, filter_wave, filter_flux)
     return rest_mag
+
+
+@jjit
+def _calc_weighted_obs_mag_from_diffstar_params_const_zmet_dust(
+    t_obs,
+    z_obs,
+    Om0,
+    Ode0,
+    w0,
+    wa,
+    h,
+    lgZsun_bin_mids,
+    log_age_gyr,
+    spec_wave,
+    spec_flux,
+    filter_wave,
+    filter_flux,
+    mah_logt0,
+    mah_logmp,
+    mah_logtc,
+    mah_k,
+    mah_early,
+    mah_late,
+    lgmcrit,
+    lgy_at_mcrit,
+    indx_k,
+    indx_lo,
+    indx_hi,
+    floor_low,
+    tau_dep,
+    lg_qt,
+    lg_qs,
+    lg_drop,
+    lg_rejuv,
+    lgmet,
+    lgmet_scatter,
+    dust_x0,
+    dust_gamma,
+    dust_ampl,
+    dust_slope,
+    dust_Av,
+):
+    mah_params = mah_logt0, mah_logmp, mah_logtc, mah_k, mah_early, mah_late
+    ms_params = lgmcrit, lgy_at_mcrit, indx_k, indx_lo, indx_hi, floor_low, tau_dep
+    q_params = lg_qt, lg_qs, lg_drop, lg_rejuv
+    _res = _calc_weighted_ssp_from_diffstar_params_const_zmet(
+        t_obs,
+        lgZsun_bin_mids,
+        log_age_gyr,
+        spec_flux,
+        mah_params,
+        ms_params,
+        q_params,
+        lgmet,
+        lgmet_scatter,
+    )
+    lgmet_weights, age_weights, weighted_ssp = _res
+    wave_micron = spec_wave / 10_000
+    att_k = sbl18_k_lambda(wave_micron, dust_x0, dust_gamma, dust_ampl, dust_slope)
+    attenuation = _flux_ratio(att_k, RV_C00, dust_Av)
+    attenuated_ssp = attenuation * weighted_ssp
+
+    obs_mag = _calc_obs_mag(
+        spec_wave, attenuated_ssp, filter_wave, filter_flux, z_obs, Om0, Ode0, w0, wa, h
+    )
+    return obs_mag, weighted_ssp, attenuation
--- dsps/tests/test_diffstar_photometry_kernels.py
+++ dsps/tests/test_diffstar_photometry_kernels.py
@@ -4,10 +4,12 @@ import numpy as np
 from ..diffstar_photometry_kernels import (
     _calc_weighted_rest_mag_from_diffstar_params_const_zmet,
     _calc_weighted_rest_mag_from_diffstar_params_const_zmet_dust,
+    _calc_weighted_obs_mag_from_diffstar_params_const_zmet_dust,
 )
 from ..sfh_model import DEFAULT_MAH_PARAMS, DEFAULT_MS_PARAMS, DEFAULT_Q_PARAMS
 from ..mzr import DEFAULT_MZR_PARAMS
 from .retrieve_fake_fsps_data import load_fake_sps_data
+from ..flat_wcdm import PLANCK15, _lookback_to_z
 
 
 def test_calc_weighted_rest_mag_from_diffstar_params_const_zmet():
@@ -75,3 +77,44 @@ def test_calc_weighted_rest_mag_from_diffstar_params_const_zmet_dust():
     args2 = (*args, *dust_params)
     mag_dust = _calc_weighted_rest_mag_from_diffstar_params_const_zmet_dust(*args2)
     assert mag_nodust < mag_dust
+
+
+def test_calc_weighted_obs_mag_from_diffstar_params_const_zmet_dust():
+
+    res = load_fake_sps_data()
+    filter_waves, filter_trans, wave_ssp, spec_ssp, lgZsun_bin_mids, log_age_gyr = res
+
+    mah_params = np.array(list(DEFAULT_MAH_PARAMS.values()))
+    ms_params = np.array(list(DEFAULT_MS_PARAMS.values()))
+    q_params = np.array(list(DEFAULT_Q_PARAMS.values()))
+    met_params = np.array(list(DEFAULT_MZR_PARAMS.values()))
+    lgmet = -1.0
+    lgmet_scatter = met_params[-1]
+
+    dust_x0, dust_gamma, dust_ampl, dust_slope, dust_Av = 0.25, 0.1, 0.5, 1.5, 1.0
+    dust_params = dust_x0, dust_gamma, dust_ampl, dust_slope, dust_Av
+
+    z_obs = 0.5
+    TODAY = 13.8
+    t_obs = TODAY - _lookback_to_z(z_obs, *PLANCK15)
+
+    args = (
+        t_obs,
+        z_obs,
+        *PLANCK15,
+        lgZsun_bin_mids,
+        log_age_gyr,
+        wave_ssp,
+        spec_ssp,
+        filter_waves[0, :],
+        filter_trans[0, :],
+        *mah_params,
+        *ms_params,
+        *q_params,
+        lgmet,
+        lgmet_scatter,
+        *dust_params,
+    )
+
+    res = _calc_weighted_obs_mag_from_diffstar_params_const_zmet_dust(*args)
+    obs_mag, weighted_ssp, attenuation = res
