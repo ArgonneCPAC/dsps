52d789e20bf341746be0d21fca63589fb333df7b
Andrew Hearin <ahearin@anl.gov>
2021-11-16 10:34:43 -0600

    Implemented constant metallicity model

--- dsps/tests/test_weighted_ssps.py
+++ dsps/tests/test_weighted_ssps.py
@@ -1,7 +1,9 @@
 """
 """
 import numpy as np
+import pytest
 from ..weighted_ssps import _calc_weighted_ssp_from_diffstar_params
+from ..weighted_ssps import _calc_weighted_ssp_from_diffstar_params_const_zmet
 from ..sfh_model import DEFAULT_MAH_PARAMS, DEFAULT_MS_PARAMS, DEFAULT_Q_PARAMS
 from ..mzr import DEFAULT_MZR_PARAMS
 
@@ -33,3 +35,42 @@ def test_calc_weighted_ssp_from_diffstar_params():
     assert lgmet_weights.shape == (n_met, 1, 1)
     assert age_weights.shape == (1, n_age, 1)
     assert mags.shape == (n_filters,)
+
+
+def test_calc_weighted_ssp_from_diffstar_params_const_zmet():
+
+    t_obs = 11.0
+    n_met, n_age, n_filters = 10, 20, 6
+    lg_ages = np.linspace(6, 10, n_age) - 9
+    ssp_templates = np.zeros((n_met, n_age, n_filters))
+    lgZsun_bin_mids = np.linspace(-2, 0, n_met)
+
+    mah_params = np.array(list(DEFAULT_MAH_PARAMS.values()))
+    ms_params = np.array(list(DEFAULT_MS_PARAMS.values()))
+    q_params = np.array(list(DEFAULT_Q_PARAMS.values()))
+    met_params = np.array(list(DEFAULT_MZR_PARAMS.values()))
+    lgmet = -1.0
+    lgmet_scatter = met_params[-1]
+
+    args = (
+        t_obs,
+        lgZsun_bin_mids,
+        lg_ages,
+        ssp_templates,
+        mah_params,
+        ms_params,
+        q_params,
+        lgmet,
+        lgmet_scatter,
+    )
+    res = _calc_weighted_ssp_from_diffstar_params_const_zmet(*args)
+    lgmet_weights, age_weights, mags = res
+    assert lgmet_weights.shape == (n_met, 1, 1)
+    assert age_weights.shape == (1, n_age, 1)
+    assert mags.shape == (n_filters,)
+
+
+@pytest.mark.xfail
+def test_tiny_scatter_edge_case():
+    """Test ensures metallicity weights are correct even when scatter is very small"""
+    raise NotImplementedError()
--- dsps/weighted_ssps.py
+++ dsps/weighted_ssps.py
@@ -5,7 +5,8 @@ from jax import numpy as jnp
 from .stellar_ages import _get_sfh_tables, _get_age_weights_from_tables
 from .stellar_ages import _get_lgt_birth, _get_lg_age_bin_edges
 from .mzr import calc_lgmet_weights_from_logsm_table
-from .mzr import calc_const_lgmet_weights
+from .mzr import _get_met_weights_singlegal, LGMET_LO, LGMET_HI
+from .utils import _get_bin_edges
 
 
 @jjit
@@ -50,6 +51,35 @@ def _calc_weighted_ssp_from_sfh_table(
 
 
 @jjit
+def _calc_weighted_ssp_from_sfh_table_const_zmet(
+    t_obs,
+    lgZsun_bin_mids,
+    lg_ages,
+    ssp_templates,
+    t_table,
+    lgt_table,
+    dt_table,
+    sfh_table,
+    logsm_table,
+    lgmet,
+    lgmet_scatter,
+):
+    n_met, n_ages, n_filters = ssp_templates.shape
+
+    lgt_birth_bin_mids, age_weights = _get_age_weights(
+        t_obs, lg_ages, lgt_table, logsm_table
+    )
+
+    lgmet_bin_edges = _get_bin_edges(lgZsun_bin_mids, LGMET_LO, LGMET_HI)
+    lgmet_weights = _get_met_weights_singlegal(lgmet, lgmet_scatter, lgmet_bin_edges)
+    lgmet_weights = lgmet_weights.reshape((n_met, 1, 1))
+    age_weights = age_weights.reshape((1, n_ages, 1))
+    w = lgmet_weights * age_weights
+    wmags = w * ssp_templates
+    return lgmet_weights, age_weights, jnp.sum(wmags, axis=(0, 1))
+
+
+@jjit
 def _calc_weighted_ssp_from_diffstar_params(
     t_obs,
     lgZsun_bin_mids,
@@ -74,4 +104,34 @@ def _calc_weighted_ssp_from_diffstar_params(
         sfh_table,
         logsm_table,
         met_params,
+    )
+
+
+@jjit
+def _calc_weighted_ssp_from_diffstar_params_const_zmet(
+    t_obs,
+    lgZsun_bin_mids,
+    lg_ages,
+    ssp_templates,
+    mah_params,
+    ms_params,
+    q_params,
+    lgmet,
+    lgmet_scatter,
+):
+    _res = _get_sfh_tables(mah_params, ms_params, q_params)
+    t_table, lgt_table, dt_table, sfh_table, logsm_table = _res
+
+    return _calc_weighted_ssp_from_sfh_table_const_zmet(
+        t_obs,
+        lgZsun_bin_mids,
+        lg_ages,
+        ssp_templates,
+        t_table,
+        lgt_table,
+        dt_table,
+        sfh_table,
+        logsm_table,
+        lgmet,
+        lgmet_scatter,
     )
