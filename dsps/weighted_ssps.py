e54b018045d84bace94c65b2f2417424d38673f3
Andrew Hearin <ahearin@anl.gov>
2021-12-05 16:21:09 -0600

    Added SSP weighting kernel for age-dependent metallicities

--- dsps/weighted_ssps.py
+++ dsps/weighted_ssps.py
@@ -6,6 +6,7 @@ from .stellar_ages import _get_sfh_tables, _get_age_weights_from_tables
 from .stellar_ages import _get_lgt_birth, _get_lg_age_bin_edges
 from .mzr import calc_lgmet_weights_from_logsm_table
 from .mzr import _get_met_weights_singlegal, LGMET_LO, LGMET_HI
+from .mzr import _get_age_correlated_met_weights
 from .utils import _get_bin_edges
 
 
@@ -80,6 +81,38 @@ def _calc_weighted_ssp_from_sfh_table_const_zmet(
 
 
 @jjit
+def _calc_weighted_flux_from_sfh_table_age_correlated_zmet(
+    t_obs,
+    lgZsun_bin_mids,
+    lg_ages,
+    ssp_flux,
+    t_table,
+    lgt_table,
+    dt_table,
+    sfh_table,
+    logsm_table,
+    lgmet_young,
+    lgmet_old,
+    lgmet_scatter,
+):
+    n_met, n_ages = ssp_flux.shape
+
+    lgt_birth_bin_mids, age_weights = _get_age_weights(
+        t_obs, lg_ages, lgt_table, logsm_table
+    )
+
+    lgmet_bin_edges = _get_bin_edges(lgZsun_bin_mids, LGMET_LO, LGMET_HI)
+    lgmet_weights = _get_age_correlated_met_weights(
+        lg_ages, lgmet_young, lgmet_old, lgmet_scatter, lgmet_bin_edges
+    )
+    lgmet_weights = lgmet_weights.T
+    age_weights = age_weights.reshape((1, n_ages))
+    w = lgmet_weights * age_weights
+    weighted_flux = w * ssp_flux
+    return lgmet_weights, age_weights, jnp.sum(weighted_flux, axis=(0, 1))
+
+
+@jjit
 def _calc_weighted_ssp_from_diffstar_params(
     t_obs,
     lgZsun_bin_mids,
@@ -135,3 +168,37 @@ def _calc_weighted_ssp_from_diffstar_params_const_zmet(
         lgmet,
         lgmet_scatter,
     )
+
+
+@jjit
+def _calc_weighted_flux_from_diffstar_age_correlated_zmet(
+    t_obs,
+    lgZsun_bin_mids,
+    lg_ages,
+    ssp_flux,
+    mah_params,
+    ms_params,
+    q_params,
+    lgmet_young,
+    lgmet_old,
+    lgmet_scatter,
+):
+    _res = _get_sfh_tables(mah_params, ms_params, q_params)
+    t_table, lgt_table, dt_table, sfh_table, logsm_table = _res
+
+    _res = _calc_weighted_flux_from_sfh_table_age_correlated_zmet(
+        t_obs,
+        lgZsun_bin_mids,
+        lg_ages,
+        ssp_flux,
+        t_table,
+        lgt_table,
+        dt_table,
+        sfh_table,
+        logsm_table,
+        lgmet_young,
+        lgmet_old,
+        lgmet_scatter,
+    )
+    lgmet_weights, age_weights, weighted_flux = _res
+    return lgmet_weights, age_weights, weighted_flux
