5ce3a42f12da0e3e6773721f0366c583712225a2
Andrew Hearin <ahearin@anl.gov>
2021-12-05 17:45:58 -0600

    Added unit test of new function

--- dsps/tests/test_diffstar_photometry_kernels.py
+++ dsps/tests/test_diffstar_photometry_kernels.py
@@ -5,6 +5,7 @@ from ..diffstar_photometry_kernels import (
     _calc_weighted_rest_mag_from_diffstar_params_const_zmet,
     _calc_weighted_rest_mag_from_diffstar_params_const_zmet_dust,
     _calc_weighted_obs_mag_from_diffstar_params_const_zmet_dust,
+    _calc_weighted_rest_mag_from_diffstar_params_age_correlated_zmet,
 )
 from ..sfh_model import DEFAULT_MAH_PARAMS, DEFAULT_MS_PARAMS, DEFAULT_Q_PARAMS
 from ..mzr import DEFAULT_MZR_PARAMS
@@ -118,3 +119,34 @@ def test_calc_weighted_obs_mag_from_diffstar_params_const_zmet_dust():
 
     res = _calc_weighted_obs_mag_from_diffstar_params_const_zmet_dust(*args)
     obs_mag, weighted_ssp, attenuation = res
+
+
+def test_calc_weighted_rest_mag_from_diffstar_params_age_correlated_zmet():
+    res = load_fake_sps_data()
+    filter_waves, filter_trans, wave_ssp, spec_ssp, lgZsun_bin_mids, log_age_gyr = res
+    t_obs = 11.0
+
+    mah_params = np.array(list(DEFAULT_MAH_PARAMS.values()))
+    ms_params = np.array(list(DEFAULT_MS_PARAMS.values()))
+    q_params = np.array(list(DEFAULT_Q_PARAMS.values()))
+    met_params = np.array(list(DEFAULT_MZR_PARAMS.values()))
+    lgmet_young, lgmet_old = -1.0, -1.0
+    lgmet_scatter = met_params[-1]
+
+    args = (
+        t_obs,
+        lgZsun_bin_mids,
+        log_age_gyr,
+        wave_ssp,
+        spec_ssp,
+        filter_waves[0, :],
+        filter_trans[0, :],
+        *mah_params,
+        *ms_params,
+        *q_params,
+        lgmet_young,
+        lgmet_old,
+        lgmet_scatter,
+    )
+    mag = _calc_weighted_rest_mag_from_diffstar_params_age_correlated_zmet(*args)
+    assert 0 < mag < 10
