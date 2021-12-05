ae074d940ac51741b654e7f60d642e9d3cd4ae31
Andrew Hearin <ahearin@anl.gov>
2021-12-05 17:43:33 -0600

    Added untested implementation of _calc_weighted_rest_mag_from_diffstar_params_age_correlated_zmet function

--- dsps/diffstar_photometry_kernels.py
+++ dsps/diffstar_photometry_kernels.py
@@ -1,8 +1,11 @@
 """
 """
 from jax import jit as jjit
+from jax import vmap
+from jax import numpy as jnp
 from .photometry_kernels import _calc_rest_mag, _calc_obs_mag
 from .weighted_ssps import _calc_weighted_ssp_from_diffstar_params_const_zmet
+from .weighted_ssps import _calc_weighted_flux_from_diffstar_age_correlated_zmet
 from .attenuation_kernels import _flux_ratio, sbl18_k_lambda, RV_C00
 
 
@@ -47,6 +50,65 @@ def _calc_weighted_rest_mag_from_diffstar_params_const_zmet(
         ms_params,
         q_params,
         lgmet,
+        lgmet_scatter,
+    )
+    lgmet_weights, age_weights, weighted_ssp = _res
+
+    rest_mag = _calc_rest_mag(spec_wave, weighted_ssp, filter_wave, filter_flux)
+    return rest_mag
+
+
+_a = (None, None, None, 0, None, None, None, None, None, None)
+_calc_weighted_flux_from_diffstar_age_correlated_zmet_vmap = jjit(
+    vmap(_calc_weighted_flux_from_diffstar_age_correlated_zmet, in_axes=_a)
+)
+
+
+@jjit
+def _calc_weighted_rest_mag_from_diffstar_params_age_correlated_zmet(
+    t_obs,
+    lgZsun_bin_mids,
+    log_age_gyr,
+    spec_wave,
+    spec_flux,
+    filter_wave,
+    filter_flux,
+    mah_logt0,
+    mah_logmp,
+    mah_logtc,
+    mah_k,
+    mah_early,
+    mah_late,
+    lgmcrit,
+    lgy_at_mcrit,
+    indx_k,
+    indx_lo,
+    indx_hi,
+    floor_low,
+    tau_dep,
+    lg_qt,
+    lg_qs,
+    lg_drop,
+    lg_rejuv,
+    lgmet_young,
+    lgmet_old,
+    lgmet_scatter,
+):
+    mah_params = mah_logt0, mah_logmp, mah_logtc, mah_k, mah_early, mah_late
+    ms_params = lgmcrit, lgy_at_mcrit, indx_k, indx_lo, indx_hi, floor_low, tau_dep
+    q_params = lg_qt, lg_qs, lg_drop, lg_rejuv
+
+    spec_flux = jnp.moveaxis(spec_flux, 2, 0)
+    _res = _calc_weighted_flux_from_diffstar_age_correlated_zmet_vmap(
+        t_obs,
+        lgZsun_bin_mids,
+        log_age_gyr,
+        spec_flux,
+        mah_params,
+        ms_params,
+        q_params,
+        lgmet_young,
+        lgmet_old,
         lgmet_scatter,
     )
     lgmet_weights, age_weights, weighted_ssp = _res
