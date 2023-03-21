4a1043d2b265ee8894c1f8b1bae9f0fe9c77ce87
Andrew Hearin <ahearin@anl.gov>
2023-03-21 13:46:40 -0500

    Remove diffstar photometry modules

--- dsps/__init__.py
+++ dsps/__init__.py
@@ -3,6 +3,3 @@
 """
 from ._version import __version__
 from .seds_from_tables import *
-from .seds_from_diffstar import *
-from .restmag_from_diffstar import *
-from .obsmag_from_diffstar import *
--- dsps/tests/test_dsps_seds.py
+++ dsps/tests/test_dsps_seds.py
@@ -6,8 +6,6 @@ from collections import namedtuple
 import numpy as np
 from ..seds_from_tables import compute_sed_galpop
 from ..load_fsps_data import TASSO_DRN, load_fsps_testing_data
-from ..seds_from_diffstar import compute_diffstarpop_restframe_seds
-from ..restmag_from_diffstar import compute_diffstarpop_restframe_mags
 
 
 if os.path.isdir(TASSO_DRN):
@@ -83,90 +81,3 @@ def test_compute_sed_galpop_from_table():
     )
     seds, logsm_tables = compute_sed_galpop(*sed_args)
     assert seds.shape == (n_pop, ssp_flux.shape[-1])
-
-
-@pytest.mark.skipif("not HAS_FSPS_TEST_DATA")
-def test_compute_sed_galpop_from_diffstar():
-    testing_data = _get_testing_data()
-    lgZsun_bin_mids = testing_data.lgZsun_bin_mids
-    log_age_gyr = testing_data.log_age_gyr
-    ssp_wave = testing_data.ssp_wave
-    ssp_flux = testing_data.ssp_flux
-    n_pop = 50
-
-    diffmah_params = np.ones(shape=(n_pop, 6))
-    u_ms_params = np.zeros(shape=(n_pop, 5))
-    u_q_params = np.zeros(shape=(n_pop, 4))
-    met_params = np.ones(shape=(n_pop, 2))
-
-    t_obs = 13.0
-
-    dstar_sed_args = (
-        t_obs,
-        lgZsun_bin_mids,
-        log_age_gyr,
-        ssp_wave,
-        ssp_flux,
-        diffmah_params,
-        u_ms_params,
-        u_q_params,
-        met_params,
-    )
-    seds, sfh_tables, logsm_tables = compute_diffstarpop_restframe_seds(*dstar_sed_args)
-    assert seds.shape == (n_pop, ssp_flux.shape[-1])
-
-    # Test attenuation
-    x0, gamma, ampl, slope1, Av = 0.0, 0.1, 0.5, -0.5, 1.0
-    dust_params = x0, gamma, ampl, slope1, Av
-    n_dust = len(dust_params)
-    dust_params_pop = np.tile(dust_params, n_pop).reshape((n_pop, n_dust))
-    seds, sfh_tables, logsm_tables = compute_diffstarpop_restframe_seds(
-        *dstar_sed_args, dust_params=dust_params_pop
-    )
-    assert seds.shape == (n_pop, ssp_flux.shape[-1])
-
-
-@pytest.mark.skipif("not HAS_FSPS_TEST_DATA")
-def test_compute_restframe_mags_galpop_from_diffstar():
-    testing_data = _get_testing_data()
-    lgZsun_bin_mids = testing_data.lgZsun_bin_mids
-    log_age_gyr = testing_data.log_age_gyr
-    ssp_wave = testing_data.ssp_wave
-    ssp_flux = testing_data.ssp_flux
-    filter_waves = testing_data.filter_waves
-    filter_trans = testing_data.filter_trans
-    n_pop = 50
-    n_filters = filter_trans.shape[0]
-
-    diffmah_params = np.ones(shape=(n_pop, 6))
-    u_ms_params = np.zeros(shape=(n_pop, 5))
-    u_q_params = np.zeros(shape=(n_pop, 4))
-    met_params = np.ones(shape=(n_pop, 2))
-
-    t_obs = 13.0
-    rest_args = (
-        t_obs,
-        lgZsun_bin_mids,
-        log_age_gyr,
-        ssp_wave,
-        ssp_flux,
-        diffmah_params,
-        u_ms_params,
-        u_q_params,
-        met_params,
-        filter_waves,
-        filter_trans,
-    )
-
-    mags_rest_subsample = compute_diffstarpop_restframe_mags(*rest_args)
-    assert mags_rest_subsample.shape == (n_pop, n_filters)
-
-    # Test attenuation
-    x0, gamma, ampl, slope1, Av = 0.0, 0.1, 0.5, -0.5, 1.0
-    dust_params = x0, gamma, ampl, slope1, Av
-    n_dust = len(dust_params)
-    dust_params_pop = np.tile(dust_params, n_pop).reshape((n_pop, n_dust))
-    mags_rest_pop = compute_diffstarpop_restframe_mags(
-        *rest_args, dust_params=dust_params_pop
-    )
-    assert mags_rest_pop.shape == (n_pop, n_filters)
