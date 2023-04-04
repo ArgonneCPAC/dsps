20973c858cbdc15352ab7679dd770a1a7005855d
Andrew Hearin <ahearin@anl.gov>
2023-04-04 10:44:22 -0500

    Add _calc_rest_sed_met_table function

--- dsps/csp_seds/composite_sed.py
+++ dsps/csp_seds/composite_sed.py
@@ -2,7 +2,7 @@
 """
 from jax import jit as jjit
 from jax import numpy as jnp
-from .ssp_weights import _calc_ssp_weights_lognormal_mdf
+from .ssp_weights import _calc_ssp_weights_lognormal_mdf, _calc_ssp_weights_met_table
 from .stellar_age_weights import _calc_logsm_table_from_sfh_table
 from ..constants import SFR_MIN
 
@@ -21,6 +21,41 @@ def _calc_rest_sed_lognormal_mdf(
 ):
     weights, age_weights, lgmet_weights = _calc_ssp_weights_lognormal_mdf(
         t_obs, gal_t_table, gal_sfr_table, ssp_lg_age, ssp_lgmet, lgmet, lgmet_scatter
+    )
+    n_met, n_ages = weights.shape
+    weights = weights.reshape((n_met, n_ages, 1))
+    sed_unit_mstar = jnp.sum(ssp_flux * weights, axis=(0, 1))
+
+    lgt_obs = jnp.log10(t_obs)
+    lgt_table = jnp.log10(gal_t_table)
+    logsm_table = _calc_logsm_table_from_sfh_table(gal_t_table, gal_sfr_table, sfr_min)
+    logsm_obs = jnp.interp(lgt_obs, lgt_table, logsm_table)
+    mstar_obs = 10**logsm_obs
+
+    rest_sed = sed_unit_mstar * mstar_obs
+    return rest_sed
+
+
+@jjit
+def _calc_rest_sed_met_table(
+    t_obs,
+    gal_t_table,
+    gal_sfr_table,
+    ssp_lg_age,
+    ssp_lgmet,
+    ssp_flux,
+    gal_lgmet_table,
+    lgmet_scatter,
+    sfr_min=SFR_MIN,
+):
+    weights, age_weights, lgmet_weights = _calc_ssp_weights_met_table(
+        t_obs,
+        gal_t_table,
+        gal_sfr_table,
+        ssp_lg_age,
+        ssp_lgmet,
+        gal_lgmet_table,
+        lgmet_scatter,
     )
     n_met, n_ages = weights.shape
     weights = weights.reshape((n_met, n_ages, 1))
--- dsps/csp_seds/tests/test_composite_sed.py
+++ dsps/csp_seds/tests/test_composite_sed.py
@@ -2,7 +2,7 @@
 """
 import numpy as np
 from jax import random as jran
-from ..composite_sed import _calc_rest_sed_lognormal_mdf
+from ..composite_sed import _calc_rest_sed_lognormal_mdf, _calc_rest_sed_met_table
 from ...constants import T_BIRTH_MIN
 
 SEED = 43
@@ -42,5 +42,42 @@ def test_calc_rest_sed_lognormal_mdf():
         lgmet_scatter,
     )
     sed = _calc_rest_sed_lognormal_mdf(*args)
+    assert sed.shape == (n_wave,)
+    assert np.any(sed > 0)
+
+
+def test_calc_rest_sed_lgmet_table():
+    ran_key = jran.PRNGKey(SEED)
+    t_obs = 13.0
+    n_t = 500
+    gal_t_table = np.linspace(T_BIRTH_MIN, t_obs, n_t)
+
+    sfr_key, met_key, sed_key = jran.split(ran_key, 3)
+    gal_sfr_table = jran.uniform(sfr_key, minval=0, maxval=10, shape=(n_t,))
+
+    n_ages = FSPS_LG_AGES.size
+    ssp_lg_age = FSPS_LG_AGES - 9.0
+    n_met = 15
+    ssp_lgmet = np.linspace(-4, 0.5, n_met)
+
+    gal_lgmet_table = jran.uniform(
+        met_key, minval=ssp_lgmet.min(), maxval=ssp_lgmet.max(), shape=(n_t,)
+    )
+    lgmet_scatter = 0.1
+
+    n_wave = 20
+    ssp_flux = jran.uniform(sed_key, minval=0, maxval=1, shape=(n_met, n_ages, n_wave))
+
+    args = (
+        t_obs,
+        gal_t_table,
+        gal_sfr_table,
+        ssp_lg_age,
+        ssp_lgmet,
+        ssp_flux,
+        gal_lgmet_table,
+        lgmet_scatter,
+    )
+    sed = _calc_rest_sed_met_table(*args)
     assert sed.shape == (n_wave,)
     assert np.any(sed > 0)
