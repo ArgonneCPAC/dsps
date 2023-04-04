b786d67fd0cc307c33dc1cf88faeefdf120a8ef8
Andrew Hearin <ahearin@anl.gov>
2023-04-04 12:24:48 -0500

    Add docstring to composite_sed.py functions

--- dsps/csp_seds/composite_sed.py
+++ dsps/csp_seds/composite_sed.py
@@ -19,6 +19,42 @@ def _calc_rest_sed_lognormal_mdf(
     t_obs,
     sfr_min=SFR_MIN,
 ):
+    """
+    Calculate the SED of a galaxy defined by input tables of SFH and metallicity
+
+    Parameters
+    ----------
+    gal_t_table : ndarray of shape (n_t, )
+        Age of the universe in Gyr at which the input galaxy SFH and metallicity
+        have been tabulated
+
+    gal_sfr_table : ndarray of shape (n_t, )
+        Star formation history in Msun/yr evaluated at the input gal_t_table
+
+    gal_lgmet : ndarray of shape (n_t, )
+        Metallicity of the galaxy at the time of observation
+
+    gal_lgmet_scatter : float
+        Lognormal scatter in metallicity
+
+    ssp_lg_age : ndarray of shape (n_ages, )
+        Age of stellar populations of the input SSP table ssp_flux
+
+    ssp_lgmet : ndarray of shape (n_met, )
+        Metallicity of stellar populations of the input SSP table ssp_flux
+
+    ssp_flux : ndarray of shape (n_met, n_ages, n_wave)
+        SED of the SSP in units of Lsun/Hz/Msun
+
+    t_obs : float
+        Age of the universe in Gyr at the time the galaxy is observed
+
+    Returns
+    -------
+    rest_sed : ndarray of shape (n_wave, )
+        Restframe SED of the galaxy in units of Lsun/Hz
+
+    """
     weights, age_weights, lgmet_weights = _calc_ssp_weights_lognormal_mdf(
         gal_t_table,
         gal_sfr_table,
@@ -54,6 +90,42 @@ def _calc_rest_sed_met_table(
     t_obs,
     sfr_min=SFR_MIN,
 ):
+    """
+    Calculate the SED of a galaxy defined by input tables of SFH and metallicity
+
+    Parameters
+    ----------
+    gal_t_table : ndarray of shape (n_t, )
+        Age of the universe in Gyr at which the input galaxy SFH and metallicity
+        have been tabulated
+
+    gal_sfr_table : ndarray of shape (n_t, )
+        Star formation history in Msun/yr evaluated at the input gal_t_table
+
+    gal_lgmet_table : ndarray of shape (n_t, )
+        Metallicity history evaluated at the input gal_t_table
+
+    gal_lgmet_scatter : float
+        Lognormal scatter in metallicity
+
+    ssp_lg_age : ndarray of shape (n_ages, )
+        Age of stellar populations of the input SSP table ssp_flux
+
+    ssp_lgmet : ndarray of shape (n_met, )
+        Metallicity of stellar populations of the input SSP table ssp_flux
+
+    ssp_flux : ndarray of shape (n_met, n_ages, n_wave)
+        SED of the SSP in units of Lsun/Hz/Msun
+
+    t_obs : float
+        Age of the universe in Gyr at the time the galaxy is observed
+
+    Returns
+    -------
+    rest_sed : ndarray of shape (n_wave, )
+        Restframe SED of the galaxy in units of Lsun/Hz
+
+    """
     weights, age_weights, lgmet_weights = _calc_ssp_weights_met_table(
         gal_t_table,
         gal_sfr_table,
