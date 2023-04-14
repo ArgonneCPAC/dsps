64215c140e2d8a03bf66b59efe065720e37cd80f
Andrew Hearin <ahearin@anl.gov>
2023-04-14 11:54:25 -0500

    Use tool.setuptools_scm to write version to now-deleted dsps/_version.py

--- setup.py
+++ setup.py
@@ -1,16 +1,8 @@
-import os
 from setuptools import setup, find_packages
-
-
-__version__ = None
-pth = os.path.join(os.path.dirname(os.path.realpath(__file__)), "dsps", "_version.py")
-with open(pth, "r") as fp:
-    exec(fp.read())
 
 
 setup(
     name="dsps",
-    version=__version__,
     author="Andrew Hearin",
     author_email="ahearin@anl.gov",
     description="Differentiable Stellar Population Synthesis",
