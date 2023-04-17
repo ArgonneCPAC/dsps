c0a82664f6e12a051cf074ae5322ab8699987dee
Andrew Hearin <ahearin@anl.gov>
2023-04-17 17:47:47 -0500

    Move SSPData into defaults.py

--- dsps/data_loaders/defaults.py
+++ dsps/data_loaders/defaults.py
@@ -1,5 +1,32 @@
 """Globals storing defaults for various DSPS options"""
+import typing
+import numpy as np
 
 
 DEFAULT_SSP_BNAME = "ssp_data_fsps_v3.2_lgmet_age.h5"
-DEFAULT_SSP_KEYS = ("lgmet", "lg_age", "wave", "flux")
+
+
+class SSPData(typing.NamedTuple):
+    """NamedTuple with info about SSP templates
+
+    ssp_lgmet : ndarray of shape (n_met, )
+        Array of log10(Z) of the SSP templates
+        where dimensionless Z is the mass fraction of elements heavier than He
+
+    ssp_lg_age : ndarray of shape (n_ages, )
+        Array of log10(age/Gyr) of the SSP templates
+
+    ssp_wave : ndarray of shape (n_wave, )
+
+    ssp_flux : ndarray of shape (n_met, n_ages, n_wave)
+        SED of the SSP in units of Lsun/Hz/Msun
+
+    """
+
+    ssp_lgmet: np.ndarray
+    ssp_lg_age: np.ndarray
+    ssp_wave: np.ndarray
+    ssp_flux: np.ndarray
+
+
+DEFAULT_SSP_KEYS = SSPData._fields
--- dsps/data_loaders/load_ssp_data.py
+++ dsps/data_loaders/load_ssp_data.py
@@ -1,35 +1,10 @@
 """
 """
-import typing
 from collections import OrderedDict
 import os
 import h5py
-import numpy as np
 
-from .defaults import DEFAULT_SSP_BNAME, DEFAULT_SSP_KEYS
-
-
-class SSPData(typing.NamedTuple):
-    """NamedTuple with info about SSP templates
-
-    ssp_lgmet : ndarray of shape (n_met, )
-        Array of log10(Z) of the SSP templates
-        where dimensionless Z is the mass fraction of elements heavier than He
-
-    ssp_lg_age : ndarray of shape (n_ages, )
-        Array of log10(age/Gyr) of the SSP templates
-
-    ssp_wave : ndarray of shape (n_wave, )
-
-    ssp_flux : ndarray of shape (n_met, n_ages, n_wave)
-        SED of the SSP in units of Lsun/Hz/Msun
-
-    """
-
-    ssp_lgmet: np.ndarray
-    ssp_lg_age: np.ndarray
-    ssp_wave: np.ndarray
-    ssp_flux: np.ndarray
+from .defaults import DEFAULT_SSP_BNAME, DEFAULT_SSP_KEYS, SSPData
 
 
 def load_default_ssp_templates(
