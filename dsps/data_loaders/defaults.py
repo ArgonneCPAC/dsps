cea0be4548231bf87c6b5e0499cc318498460cb5
Andrew Hearin <ahearin@anl.gov>
2023-04-17 16:59:55 -0500

    Progress on load_default_ssp_templates function

---
+++ dsps/data_loaders/defaults.py
@@ -0,0 +1,5 @@
+"""Globals storing defaults for various DSPS options"""
+
+
+DEFAULT_SSP_BNAME = "fsps_v3.2_lgmet_age_ssp_data.h5"
+DEFAULT_SSP_KEYS = ("lgmet", "lg_age", "wave", "flux")
--- dsps/data_loaders/load_ssp_data.py
+++ dsps/data_loaders/load_ssp_data.py
@@ -1,16 +1,69 @@
 """
 """
+import typing
 from collections import OrderedDict
 import os
 import h5py
+import numpy as np
+
+from .defaults import DEFAULT_SSP_BNAME, DEFAULT_SSP_KEYS
 
 
-def load_default_ssp_templates(drn=None, bn=None):
-    if drn is None:
-        drn = os.environ["DSPS_DRN"]
-    if bn is None:
-        raise NotImplementedError("WIP")
-    fn = os.path.join(drn, bn)
+class SSPData(typing.NamedTuple):
+    """NamedTuple with info about SSP templates
+
+    ssp_lgmet : ndarray of shape (n_met, )
+        Array of log10(Z) of the SSP templates
+        where dimensionless Z is the mass fraction of elements heavier than He
+
+    ssp_lg_age : ndarray of shape (n_ages, )
+        Array of log10(age/Gyr) of the SSP templates
+
+    ssp_wave : ndarray of shape (n_wave, )
+
+    ssp_flux : ndarray of shape (n_met, n_ages, n_wave)
+        SED of the SSP in units of Lsun/Hz/Msun
+
+    """
+
+    ssp_lgmet: np.ndarray
+    ssp_lg_age: np.ndarray
+    ssp_wave: np.ndarray
+    ssp_flux: np.ndarray
+
+
+def load_default_ssp_templates(
+    fn=None,
+    drn=None,
+    bn=DEFAULT_SSP_BNAME,
+    ssp_keys=DEFAULT_SSP_KEYS,
+):
+    """Load SSP templates from disk, defaulting to DSPS package data location
+
+    Parameters
+    ----------
+    fn : string, optional
+        Absolute path to hdf5 file storing the SSP data.
+        This argument supersedes drn and bn.
+        Default is None, in which case DSPS will look
+        in the directory stored in the DSPS_DRN environment variable
+
+    drn : string, optional
+        Directory to hdf5 file storing the SSP data
+        This argument is only used if fn is not supplied
+        Default behavior is the DSPS_DRN environment variable
+
+    bn : string, optional
+        Basename of hdf5 file storing the SSP data
+        This argument is only used if fn is not supplied
+
+    """
+    if fn is None:
+        if drn is None:
+            drn = os.environ["DSPS_DRN"]
+        if bn is None:
+            raise NotImplementedError("WIP")
+        fn = os.path.join(drn, bn)
     assert os.path.isfile(fn), "{0} does not exist".format(fn)
 
     ssp_data = OrderedDict()
@@ -18,4 +71,4 @@ def load_default_ssp_templates(drn=None, bn=None):
         for key in hdf:
             ssp_data[key] = hdf[key][...]
 
-    return ssp_data
+    return SSPData(*[ssp_data[key] for key in ssp_keys])
