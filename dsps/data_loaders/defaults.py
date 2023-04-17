15a9be2267d74b57c28c525dda06d69b8c5308f6
Andrew Hearin <ahearin@anl.gov>
2023-04-17 17:33:54 -0500

    Update defaults

--- dsps/data_loaders/defaults.py
+++ dsps/data_loaders/defaults.py
@@ -1,5 +1,5 @@
 """Globals storing defaults for various DSPS options"""
 
 
-DEFAULT_SSP_BNAME = "fsps_v3.2_lgmet_age_ssp_data.h5"
+DEFAULT_SSP_BNAME = "ssp_data_fsps_v3.2_lgmet_age.h5"
 DEFAULT_SSP_KEYS = ("lgmet", "lg_age", "wave", "flux")
