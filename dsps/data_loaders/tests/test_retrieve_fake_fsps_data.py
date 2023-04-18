f8dfdc5b4c8f7fc4af8292a454d0ef3dc4f91b20
Andrew Hearin <ahearin@anl.gov>
2023-04-18 10:22:12 -0500

    Improve testing of fake SPS data loader

--- dsps/data_loaders/tests/test_retrieve_fake_fsps_data.py
+++ dsps/data_loaders/tests/test_retrieve_fake_fsps_data.py
@@ -1,8 +1,18 @@
 """
 """
+import numpy as np
 from ..retrieve_fake_fsps_data import load_fake_sps_data
 
 
 def test_load_fake_sps_data():
-    ret = load_fake_sps_data()
-    filter_waves, filter_trans, wave_ssp, spec_ssp, lgZsun_bin_mids, log_age_gyr = ret
+    ssp_data = load_fake_sps_data()
+    (n_met, n_age, n_wave) = ssp_data.ssp_flux.shape
+
+    assert ssp_data.ssp_lgmet.shape == (n_met,)
+    assert ssp_data.ssp_lg_age.shape == (n_age,)
+    assert ssp_data.ssp_wave.shape == (n_wave,)
+
+    assert np.all(np.isfinite(ssp_data.ssp_lgmet))
+    assert np.all(np.isfinite(ssp_data.ssp_lg_age))
+    assert np.all(np.isfinite(ssp_data.ssp_wave))
+    assert np.all(np.isfinite(ssp_data.ssp_flux))
