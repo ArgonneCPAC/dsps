cbe7f749f5cf0fefac11596310b33d13b9c9be1a
Andrew Hearin <ahearin@anl.gov>
2023-04-18 15:46:48 -0500

    Add calc_rest_sed_sfh_table_lognormal_mdf to sed.__init__

--- dsps/__init__.py
+++ dsps/__init__.py
@@ -4,3 +4,4 @@
 from ._version import __version__
 
 from .data_loaders import load_ssp_templates
+from .sed import calc_rest_sed_sfh_table_lognormal_mdf
---
+++ dsps/sed/__init__.py
@@ -0,0 +1,5 @@
+# flake8: noqa
+"""
+"""
+from .csp_sed import calc_rest_sed_sfh_table_lognormal_mdf
+from .csp_sed import calc_rest_sed_sfh_table_met_table
