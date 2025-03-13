0756b7a5bbccbd4b96b8fb8a50b1849e7dc2e29f
Andrew Hearin <ahearin@anl.gov>
2025-03-13 14:11:59 -0500

    Undo changes to photpop.py

--- dsps/photometry/photpop.py
+++ dsps/photometry/photpop.py
@@ -87,61 +87,6 @@ _calc_rest_mag_vmap_f_ssp = jjit(
 
 
 @jjit
-def precompute_ssp_obsmags(
-    ssp_wave, ssp_fluxes, filter_waves, filter_trans, z_obs, Om0, w0, wa, h
-):
-    """Precompute observed magnitudes of a collection of SEDs on a redshift grid
-
-    Parameters
-    ----------
-    ssp_wave : array of shape (n_spec, )
-        Wavelength of the SEDs in Angstroms
-
-    ssp_fluxes : array of shape (n_met, n_age, n_spec)
-        Flux of the SEDs in Lsun/Hz, normalized to unit stellar mass
-
-    filter_waves : array of shape (n_filters, n_trans_curve)
-        Wavelength of the filter transmission curves in Angstroms
-
-    filter_trans : array of shape (n_filters, n_trans_curve)
-        Transmission curves defining fractional transmission of the filters
-
-    z_obs : float
-        Redshift at which the magnitudes will be computed
-
-    Om0 : float
-        Cosmological matter density at z=0
-
-    w0 : float
-        Dark energy equation of state parameter
-
-    wa : float
-        Dark energy equation of state parameter
-
-    h : float
-        Hubble parameter
-
-    Returns
-    -------
-    ssp_photmag_table : array of shape (n_met, n_age, n_filters)
-
-    """
-    ssp_obsmag_table = _calc_obs_mag_vmap_f_ssp(
-        ssp_wave, ssp_fluxes, filter_waves, filter_trans, z_obs, Om0, w0, wa, h
-    )
-    return ssp_obsmag_table
-
-
-_calc_rest_mag_vmap_f = jjit(vmap(calc_rest_mag, in_axes=[None, None, 0, 0]))
-_calc_rest_mag_vmap_f_ssp = jjit(
-    vmap(
-        vmap(_calc_rest_mag_vmap_f, in_axes=[None, 0, None, None]),
-        in_axes=[None, 0, None, None],
-    )
-)
-
-
-@jjit
 def precompute_ssp_restmags(ssp_wave, ssp_fluxes, filter_waves, filter_trans):
     """Precompute restframe magnitudes of a collection of SEDs
 
