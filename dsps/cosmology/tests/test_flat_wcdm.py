1875271917d1d370248e2e8279584038827f6af3
Andrew Hearin <ahearin@anl.gov>
2025-04-17 16:50:39 -0500

    Refactor testing module

--- dsps/cosmology/tests/test_flat_wcdm.py
+++ dsps/cosmology/tests/test_flat_wcdm.py
@@ -1,22 +1,9 @@
-"""
-"""
+""" """
 
 import numpy as np
 import pytest
 
-from ..flat_wcdm import PLANCK15 as PLANCK15_dsps
-from ..flat_wcdm import WMAP5 as WMAP5_dsps
-from ..flat_wcdm import (
-    CosmoParams,
-    _delta_vir,
-    _Om,
-    age_at_z,
-    age_at_z0,
-    angular_diameter_distance,
-    distance_modulus,
-    lookback_time,
-    virial_dynamical_time,
-)
+from .. import flat_wcdm
 
 try:
     from astropy.cosmology import WMAP5 as WMAP5_astropy
@@ -25,7 +12,7 @@ try:
 
     HAS_ASTROPY = True
     ASTROPY_COSMO_LIST = [Planck15_astropy, WMAP5_astropy]
-    DSPS_COSMO_LIST = [PLANCK15_dsps, WMAP5_dsps]
+    DSPS_COSMO_LIST = [flat_wcdm.PLANCK15, flat_wcdm.WMAP5]
 except ImportError:
     HAS_ASTROPY = False
 
@@ -42,19 +29,19 @@ def test_predefined_cosmologies():
         assert np.allclose(cosmo_astropy.h, cosmo_dsps.h, rtol=1e-3)
 
         dmod_astropy = cosmo_astropy.distmod(zray).value
-        dmod_jax = distance_modulus(zray, *cosmo_dsps)
+        dmod_jax = flat_wcdm.distance_modulus(zray, *cosmo_dsps)
         assert np.allclose(dmod_astropy, dmod_jax, rtol=0.001)
 
         angdist_astropy = cosmo_astropy.angular_diameter_distance(zray).value
-        angdist_jax = angular_diameter_distance(zray, *cosmo_dsps)
+        angdist_jax = flat_wcdm.angular_diameter_distance(zray, *cosmo_dsps)
         assert np.allclose(angdist_astropy, angdist_jax, rtol=0.005)
 
         lookback_astropy = cosmo_astropy.lookback_time(zray).value
-        lookback_jax = lookback_time(zray, *cosmo_dsps)
+        lookback_jax = flat_wcdm.lookback_time(zray, *cosmo_dsps)
         assert np.allclose(lookback_astropy, lookback_jax, rtol=0.005)
 
         om_astropy = cosmo_astropy.Om(zray)
-        om_jax = _Om(zray, *cosmo_dsps[:-1])
+        om_jax = flat_wcdm._Om(zray, *cosmo_dsps[:-1])
         assert np.allclose(om_astropy, om_jax, rtol=0.01)
 
 
@@ -62,21 +49,21 @@ def test_predefined_cosmologies():
 def test_flat_lcdm():
     zray = np.linspace(0.001, 10, 500)
     for cosmo in ASTROPY_COSMO_LIST:
-        cosmo_dsps = CosmoParams(cosmo.Om0, -1, 0, cosmo.h)
+        cosmo_dsps = flat_wcdm.CosmoParams(cosmo.Om0, -1, 0, cosmo.h)
         dmod_astropy = cosmo.distmod(zray).value
-        dmod_jax = distance_modulus(zray, *cosmo_dsps)
+        dmod_jax = flat_wcdm.distance_modulus(zray, *cosmo_dsps)
         assert np.allclose(dmod_astropy, dmod_jax, rtol=0.001)
 
         angdist_astropy = cosmo.angular_diameter_distance(zray).value
-        angdist_jax = angular_diameter_distance(zray, *cosmo_dsps)
+        angdist_jax = flat_wcdm.angular_diameter_distance(zray, *cosmo_dsps)
         assert np.allclose(angdist_astropy, angdist_jax, rtol=0.005)
 
         lookback_astropy = cosmo.lookback_time(zray).value
-        lookback_jax = lookback_time(zray, *cosmo_dsps)
+        lookback_jax = flat_wcdm.lookback_time(zray, *cosmo_dsps)
         assert np.allclose(lookback_astropy, lookback_jax, rtol=0.005)
 
         om_astropy = cosmo.Om(zray)
-        om_jax = _Om(zray, *cosmo_dsps[:-1])
+        om_jax = flat_wcdm._Om(zray, *cosmo_dsps[:-1])
         assert np.allclose(om_astropy, om_jax, rtol=0.01)
 
 
@@ -89,23 +76,23 @@ def test_flat_wcdm_distances():
             w0 = np.random.uniform(-1.5, -0.5)
             wa = np.random.uniform(-0.5, 0.5)
             w_cosmo = Flatw0waCDM(cosmo.H0, cosmo.Om0, w0=w0, wa=wa)
-            cosmo_dsps = CosmoParams(cosmo.Om0, w0, wa, cosmo.h)
+            cosmo_dsps = flat_wcdm.CosmoParams(cosmo.Om0, w0, wa, cosmo.h)
 
             dmod_astropy = w_cosmo.distmod(zray).value
 
-            dmod_jax = distance_modulus(zray, *cosmo_dsps)
+            dmod_jax = flat_wcdm.distance_modulus(zray, *cosmo_dsps)
             assert np.allclose(dmod_astropy, dmod_jax, rtol=0.001)
 
             angdist_astropy = w_cosmo.angular_diameter_distance(zray).value
-            angdist_jax = angular_diameter_distance(zray, *cosmo_dsps)
+            angdist_jax = flat_wcdm.angular_diameter_distance(zray, *cosmo_dsps)
             assert np.allclose(angdist_astropy, angdist_jax, rtol=0.001)
 
             lookback_astropy = w_cosmo.lookback_time(zray).value
-            lookback_jax = lookback_time(zray, *cosmo_dsps)
+            lookback_jax = flat_wcdm.lookback_time(zray, *cosmo_dsps)
             assert np.allclose(lookback_astropy, lookback_jax, rtol=0.001)
 
             om_astropy = w_cosmo.Om(zray)
-            om_jax = _Om(zray, *cosmo_dsps[:-1])
+            om_jax = flat_wcdm._Om(zray, *cosmo_dsps[:-1])
             assert np.allclose(om_astropy, om_jax, rtol=0.001)
 
 
@@ -114,11 +101,11 @@ def test_delta_vir():
     """Enforce delta~178 at high-z and monotonically decreases thereafter"""
     zray = np.linspace(0.001, 10, 50)
     for cosmo in ASTROPY_COSMO_LIST:
-        cosmo_dsps = CosmoParams(cosmo.Om0, -1, 0, cosmo.h)
+        cosmo_dsps = flat_wcdm.CosmoParams(cosmo.Om0, -1, 0, cosmo.h)
         z_high = np.atleast_1d(500.0)
-        delta = _delta_vir(z_high, *cosmo_dsps[:-1])
+        delta = flat_wcdm._delta_vir(z_high, *cosmo_dsps[:-1])
         assert np.allclose(delta, 178, atol=1)
-        delta_ray = _delta_vir(zray, *cosmo_dsps[:-1])
+        delta_ray = flat_wcdm._delta_vir(zray, *cosmo_dsps[:-1])
         assert np.all(np.diff(delta_ray) > 0)
 
 
@@ -127,13 +114,13 @@ def test_dynamical_time():
     """Enforce dynamical times increase as dark energy becomes operative"""
     zray = np.linspace(0.001, 10, 50)
     for cosmo in ASTROPY_COSMO_LIST:
-        cosmo_dsps = CosmoParams(cosmo.Om0, -1, 0, cosmo.h)
+        cosmo_dsps = flat_wcdm.CosmoParams(cosmo.Om0, -1, 0, cosmo.h)
 
-        tcross = virial_dynamical_time(zray, *cosmo_dsps)
+        tcross = flat_wcdm.virial_dynamical_time(zray, *cosmo_dsps)
         assert np.all(np.diff(tcross) < 0)
 
         z0 = np.atleast_1d(0.0)
-        tcross_z0 = virial_dynamical_time(z0, *cosmo_dsps)
+        tcross_z0 = flat_wcdm.virial_dynamical_time(z0, *cosmo_dsps)
         assert np.allclose(tcross_z0, 4, atol=1)
 
 
@@ -153,16 +140,21 @@ def test_top_level_imports():
 def test_age_at_z():
     zray = np.linspace(0.001, 10, 500)
     for cosmo in ASTROPY_COSMO_LIST:
-        cosmo_dsps = CosmoParams(cosmo.Om0, -1, 0, cosmo.h)
+        cosmo_dsps = flat_wcdm.CosmoParams(cosmo.Om0, -1, 0, cosmo.h)
         age_astropy = cosmo.age(zray).value
-        age_jax = age_at_z(zray, *cosmo_dsps)
+        age_jax = flat_wcdm.age_at_z(zray, *cosmo_dsps)
         assert np.allclose(age_astropy, age_jax, rtol=0.01)
 
 
 @pytest.mark.skipif(not HAS_ASTROPY, reason=NO_ASTROPY_MSG)
 def test_age_at_z0():
     for cosmo in ASTROPY_COSMO_LIST:
-        cosmo_dsps = CosmoParams(cosmo.Om0, -1, 0, cosmo.h)
+        cosmo_dsps = flat_wcdm.CosmoParams(cosmo.Om0, -1, 0, cosmo.h)
         age_astropy = cosmo.age(0.0).value
-        age_jax = age_at_z0(*cosmo_dsps)
+        age_jax = flat_wcdm.age_at_z0(*cosmo_dsps)
         assert np.allclose(age_astropy, age_jax, rtol=0.01)
+
+
+# @pytest.mark.skipif(not HAS_ASTROPY, reason=NO_ASTROPY_MSG)
+# def test_differential_comoving_volume():
+#     differential_comoving_volume(redshift, Om0, w0, wa, h)
