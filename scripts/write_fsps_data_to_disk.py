3ad8f98c71a8ad459244e4771de61ffdbc5c3aef
Kumail Zaidi <kumail.tufts@gmail.com>
2026-03-18 23:24:51 -0400

    **kwargs in write_fsps_data_to_disk

--- scripts/write_fsps_data_to_disk.py
+++ scripts/write_fsps_data_to_disk.py
@@ -9,9 +9,22 @@ from dsps.data_loaders import retrieve_ssp_data_from_fsps
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("outname", help="Name of the output file")
+
+    parser.add_argument(
+        "--gas_logz", type=float, default=None, help="Gas-phase metallicity (log Z)"
+    )
+    parser.add_argument(
+        "--gas_logu", type=float, default=None, help="Ionization parameter (log U)"
+    )
     args = parser.parse_args()
 
-    ssp_data = retrieve_ssp_data_from_fsps()
+    kwargs = {}
+    if args.gas_logz is not None:
+        kwargs["gas_logz"] = args.gas_logz
+    if args.gas_logu is not None:
+        kwargs["gas_logu"] = args.gas_logu
+
+    ssp_data = retrieve_ssp_data_from_fsps(**kwargs)
 
     with h5py.File(args.outname, "w") as hdf:
         for key, arr in zip(ssp_data._fields, ssp_data):
