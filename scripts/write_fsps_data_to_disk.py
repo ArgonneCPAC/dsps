b830e476f6e189ca96afecd8e5e3740db96b2a20
Kumail Zaidi <kumail.tufts@gmail.com>
2026-04-02 00:49:27 -0500

    fix indent

--- scripts/write_fsps_data_to_disk.py
+++ scripts/write_fsps_data_to_disk.py
@@ -32,22 +32,22 @@ if __name__ == "__main__":
     with h5py.File(args.outname, "w") as hdf:
         for key, arr in zip(DEFAULT_SSP_KEYS, ssp_data):
             hdf[key] = arr
-            if "ssp_emlines" in ssp_data._fields:
-                ssp_emline_name = list(ssp_data.ssp_emlines._fields)
-                ssp_emline_wave = [
-                    getattr(ssp_data.ssp_emlines, name).emline_wave
-                    for name in ssp_emline_name
-                ]
-                ssp_emline_luminosity = [
-                    getattr(ssp_data.ssp_emlines, name).emline_luminosity
-                    for name in ssp_emline_name
-                ]
-                ssp_emline_luminosity = np.stack(ssp_emline_luminosity, axis=-1)
+        if "ssp_emlines" in ssp_data._fields:
+            ssp_emline_name = list(ssp_data.ssp_emlines._fields)
+            ssp_emline_wave = [
+                getattr(ssp_data.ssp_emlines, name).emline_wave
+                for name in ssp_emline_name
+            ]
+            ssp_emline_luminosity = [
+                getattr(ssp_data.ssp_emlines, name).emline_luminosity
+                for name in ssp_emline_name
+            ]
+            ssp_emline_luminosity = np.stack(ssp_emline_luminosity, axis=-1)
 
-                grp = hdf.create_group("ssp_emlines")
+            grp = hdf.create_group("ssp_emlines")
 
-                dt = h5py.string_dtype(encoding="utf-8")
-                grp.create_dataset("ssp_emline_name", data=ssp_emline_name, dtype=dt)
+            dt = h5py.string_dtype(encoding="utf-8")
+            grp.create_dataset("ssp_emline_name", data=ssp_emline_name, dtype=dt)
 
-                grp.create_dataset("ssp_emline_wave", data=ssp_emline_wave)
-                grp.create_dataset("ssp_emline_luminosity", data=ssp_emline_luminosity)
+            grp.create_dataset("ssp_emline_wave", data=ssp_emline_wave)
+            grp.create_dataset("ssp_emline_luminosity", data=ssp_emline_luminosity)
