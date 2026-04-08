7699a1b2f0a0945f1b6056989cf013630a090812
Andrew Hearin <ahearin@anl.gov>
2026-04-08 17:35:01 -0500

    Add get_subset_emline_data convenience function

--- dsps/data_loaders/load_emline_info.py
+++ dsps/data_loaders/load_emline_info.py
@@ -1,5 +1,7 @@
 """"""
 
+from collections import namedtuple
+
 import numpy as np
 
 
@@ -23,3 +25,36 @@ def read_emlines_info_fsps(fn, testmode=False):
         return emline_dict, line_wave_arr
     else:
         return emline_dict
+
+
+def get_subset_emline_data(ssp_data, emline_names):
+    """Construct a new ssp_data based only a subset of the emission lines
+
+    Parameters
+    ----------
+    ssp_data : namedtuple
+
+    emline_names : list of strings
+        Each entry should appear in ssp_data.ssp_emline_wave._fields
+
+    Returns
+    -------
+    ssp_data : namedtuple
+        The ssp_emline_wave and ssp_emline_luminosity fields will be replaced
+        with only information about lines in emline_names, preserving order.
+
+    """
+    indx_lines = np.array(
+        [ssp_data.ssp_emline_wave._fields.index(name) for name in emline_names]
+    )
+    line_wave = np.array(ssp_data.ssp_emline_wave)[indx_lines]
+    EmLineWave = namedtuple("EmLineWave", emline_names)
+    line_wave = EmLineWave(*line_wave)
+
+    line_lum = ssp_data.ssp_emline_luminosity[:, :, indx_lines]
+
+    ssp_data = ssp_data._replace(
+        ssp_emline_wave=line_wave, ssp_emline_luminosity=line_lum
+    )
+
+    return ssp_data
--- dsps/data_loaders/tests/test_load_emline_info.py
+++ dsps/data_loaders/tests/test_load_emline_info.py
@@ -36,3 +36,34 @@ def test_read_emlines_info_fsps_line_wave_values():
     emline_dict, line_wave_arr = lemi.read_emlines_info_fsps(fn, testmode=True)
 
     assert np.allclose(ssp_data.ssp_emline_wave, line_wave_arr, rtol=1e-3)
+
+
+def test_get_subset_emline_data_agrees_with_full_data():
+    """Enforce that the ssp_data subset agrees with the original data"""
+    ssp_data = load_ssp_templates(
+        bn="fsps_v0.4.7_mist_c3k_a_kroupa_wNE_logGasU-2.0_logGasZ0.0.h5"
+    )
+
+    indx_sel = [3, 5, 7]
+    line_names = ssp_data.ssp_emline_wave._fields
+    emline_names = [line_names[i] for i in indx_sel]
+    # emline_names = ["Ly_5_937", "Ly_gamma_972", "He_II_1084p94A"]
+
+    ssp_data_new = lemi.get_subset_emline_data(ssp_data, emline_names)
+
+    for i_new, i_orig in enumerate(indx_sel):
+        assert np.allclose(
+            ssp_data_new.ssp_emline_wave[i_new], ssp_data.ssp_emline_wave[i_orig]
+        )
+
+    for name in emline_names:
+        assert np.allclose(
+            getattr(ssp_data_new.ssp_emline_wave, name),
+            getattr(ssp_data.ssp_emline_wave, name),
+        )
+
+    for i_new, i_orig in enumerate(indx_sel):
+        assert np.allclose(
+            ssp_data_new.ssp_emline_luminosity[:, :, i_new],
+            ssp_data.ssp_emline_luminosity[:, :, i_orig],
+        )
